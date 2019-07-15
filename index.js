18077d3085fd453b4d57d0f5b6306147a9ca7ec4
daniemart <github email address>
2019-07-15 15:35:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove(main.querySelector('main'));
+
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+//newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion";
