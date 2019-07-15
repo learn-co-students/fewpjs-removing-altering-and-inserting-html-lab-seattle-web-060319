c165e0eab5f536ad48799d62c12fef23b06f1176
Spencer Lindemuth <lindemut@gmail.com>
2019-07-15 15:01:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code here!'
+
+main = document.getElementById("main")
+main.remove("main");
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
